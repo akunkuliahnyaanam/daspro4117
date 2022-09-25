f8b57521d022f0f320efbb695b1034b720c33fc9
Khoirul Anam <112893351+akunkuliahnyaanam@users.noreply.github.com>
2022-09-25 21:16:42 +0700

    Pertemuan5

---
+++ Pertemuan5/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float r;
+    cout << "Program Untuk Menghitung Luas Lingkaran Berdasarkan Jari-Jari\nMasukan jari-jari lingkaran:";
+    cin >> r;
+    cout << "Jadi luasnya adalah " << M_PI*r << endl;
+    return 0;
+}
